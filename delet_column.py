6da5991be5b7bc0a283d3a250cf2fd6dd61cd3c7
Bablu Kumar <67088890+bablukr056@users.noreply.github.com>
2021-09-07 01:56:18 -0400

    Update delet_column.py
    Program: Delete column from the file.

--- delet_column.py
+++ delet_column.py
@@ -3,10 +3,11 @@
 import pandas as pd
   
 # making data frame from csv file
-data = pd.read_csv("morta_svm_combined.tsv", sep="\t" )
+data = pd.read_csv("filename.tsv", sep="\t" )
   
 # dropping passed columns
 data.pop("Name")
   
 # display
 print(data[0])
+    

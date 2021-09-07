1f11485de5eeb4dc1e51356ed9442580352b0731
Bablu Kumar <67088890+bablukr056@users.noreply.github.com>
2021-09-07 01:53:10 -0400

    Update columnaddation.py
    Program: addition of column in the last with 1.

--- columnaddation.py
+++ columnaddation.py
@@ -1,13 +1,11 @@
 import re
-fo = open("orfhumancoding.tsv","r", encoding ="utf8", errors="ignore")
+fo = open("filename.tsv","r", encoding ="utf8", errors="ignore")
 while True:
    line = fo.readline()
    if not line:
       break
    line = line.rstrip()
-   #line["x"]=1
-   #res = re.split(' |\t', line)
-   #b=res[0]+"\t"+res[1]+"\t"+res[2]+"\t"+res[3]+"\t"+res[4]+"\t"+res[5]#+"\t"+res[6]
+  
    print(line,"\t1")
    
 
